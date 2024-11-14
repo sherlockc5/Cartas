8c59d137f07bc35b1d472b82efafcfe2a72314f7
Hugo Vinicius <hugovincius0301@gmail.com>
2024-11-14 07:32:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Anime',
+    'qual foi o primeiro anime shounen exibido no Japão?',
+    'Foi o Astro Boy'
+)
+
+criaCartao(
+    'Filmes',
+    'Nos filmes Transformers, qual é o nome da facção de robôs vilões?',
+    'Decepticons '
+)
+
+criaCartao(
+    'Desenho',
+    'Qual personagem é um estudante brilhante que inventa diversas máquinas malucas em sua garagem??',
+    'Jimmy Neutron'
+)
+
+criaCartao(
+    'Serie',
+    'Qual é o vilão principal da 3° temporada de The Flash?',
+    'Savitar'
+)
